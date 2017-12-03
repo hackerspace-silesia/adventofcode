18b18154f07ee2767bba60c7e9cda7ebe46368ea
Firemark <marpiechula@gmail.com>
2017-12-03 14:05:48 +0100

    firemark day1 part2

--- 2017/1/firemark.exs
+++ 2017/1/firemark.exs
@@ -2,30 +2,35 @@ defmodule TaskOne do
   def start() do
     {:ok, string_data} = File.read("input_1")
     data = get_data(string_data)
-    sum = compute_sum(data)
-    IO.puts sum
+    IO.inspect(compute_sum(data, 1), label: "part1")
+    IO.inspect(compute_sum(data, div(length(data), 2)), label: "part2")
   end
 
   defp get_data(string_data) do
-    String.codepoints(string_data)
+    string_data
+    |> String.codepoints
     |> Enum.filter(fn(char) -> char != "\n" end)
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp compute_sum([digit | tail]) do
-    compute_sum(0, digit, digit, tail)
+  defp compute_sum(data, shift) do
+    compute_sum(0, length(data) - 1, shift, data)
   end
 
-  defp compute_sum(sum, first_digit, prev_digit, [digit | tail]) do
-    add_if_is_equal(sum, digit, prev_digit)
-    |> compute_sum(first_digit, digit, tail)
+  defp compute_sum(sum, 0, shift, data) do
+    add_if_is_equal(sum, 0, shift, data)
   end
 
-  defp compute_sum(sum, first_digit, prev_digit, []) do
-    add_if_is_equal(sum, first_digit, prev_digit)
+  defp compute_sum(sum, index, shift, data) do
+    sum
+    |> add_if_is_equal(index, shift, data)
+    |> compute_sum(index - 1, shift, data)
   end
 
-  defp add_if_is_equal(sum, a, b) do
+  defp add_if_is_equal(sum, index, shift, data) do
+    shifted_index = rem(index + shift, length(data))
+    a = Enum.at(data, index) 
+    b = Enum.at(data, shifted_index)
     if a == b, do: sum + a, else: sum
   end
 end
