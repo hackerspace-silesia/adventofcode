e6f23dbd11f76fe96a1bf0a68daa56d0a4351dfe
Firemark <marpiechula@gmail.com>
2017-12-11 01:42:36 +0100

    firemark day10 part2

--- 2017/10/firemark.exs
+++ 2017/10/firemark.exs
@@ -1,9 +1,9 @@
 defmodule TaskTen do
 
   def start do
-    data = load_data("input.txt")
-    IO.inspect execute_part1(data), label: "result part1"
-    IO.inspect execute_part2(data), label: "result part2"
+    filename = "input.txt"
+    IO.inspect execute_part1(filename), label: "result part1"
+    IO.inspect execute_part2(filename), label: "result part2"
   end
 
   defp load_data(filename) do
@@ -15,20 +15,46 @@ defmodule TaskTen do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp execute_part2(data) do
+  defp execute_part2(filename) do
+    use Bitwise
+    {:ok, data} = File.read(filename)
+    data = data |> String.trim |> String.to_charlist
+    data = data ++ [17, 31, 73, 47, 23]
 
+    first_list = Enum.to_list(0..255)
+    {result, _, _} = Enum.reduce(
+      1..64, 
+      {first_list, 0, 0},
+      fn _, list -> execute(data, list) end
+    )
+
+    result  
+    |> Stream.chunk(16)
+    |> Task.async_stream(fn vals -> 
+      vals
+      |> Enum.reduce(&Bitwise.bxor/2)
+      |> Integer.to_charlist(16)
+      |> to_string
+      |> String.downcase
+    end)
+    |> Stream.map(fn {:ok, chunk} -> chunk end)
+    |> Enum.join("")
   end
 
-  defp execute_part1(data) do
+  defp execute_part1(filename) do
+    data = load_data(filename)
     list = Enum.to_list(0..255)
-    {list, _, _} = execute(data, list)
+    {result, _, _} = execute(data, list)
     [a, b] = Enum.take(result, 2)
     a * b
   end
 
-  defp execute(data, list) do
-    Enum.reduce(data, {list, 0, 0}, &reverse/2)
-    list
+  defp execute(data, {list, index, skip_size}) do
+    execute(data, list, index, skip_size)
+  end
+
+  defp execute(data, list, index \\ 0, skip_size \\ 0) do
+    Enum.reduce(data, {list, index, skip_size}, &reverse/2)
   end
 
   defp reverse(reverse_size, {list, index, skip_size}) do
