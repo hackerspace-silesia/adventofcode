905f8898ce732b750942c1bfd764d0318b657cc7
Łukasz Oleś <lukaszoles@gmail.com>
2017-12-13 18:21:11 +0100

    Day 13 faster version and slover ditributed version

--- 2017/13/slm-distr.go
+++ 2017/13/slm-distr.go
@@ -30,7 +30,7 @@ func readInput() (Firewall, int) {
 }
 
 func isCaught(time, sRange int) bool {
-	return time%(2*(sRange-1)) == 0
+	return time%(sRange*2-2) == 0
 }
 
 func getSeverity(f Firewall, layers int) int {
@@ -47,24 +47,44 @@ func getSeverity(f Firewall, layers int) int {
 	return severity
 }
 
-func whenToStart(f Firewall, layers int) int {
-	time := 0
-loop:
+func checkTime(f Firewall, layers int, timeChan chan int, respChan chan int) {
 	for {
-		for layer := 0; layer < layers+1; layer++ {
+		time := <-timeChan
+		startTime := time
+		layer := 0
+		for {
 			sRange, ok := f[layer]
 			if ok {
-				if isCaught(time+layer, sRange) {
+				if isCaught(time, sRange) {
 					break
 				}
 			}
 			if layer == layers {
-				break loop
+				respChan <- startTime
+				return
 			}
+			layer++
+			time++
 		}
-		time++
 	}
-	return time
+}
+
+func whenToStart(f Firewall, layers int) int {
+	workers := 100
+	timeChan := make(chan int, workers)
+	respChan := make(chan int)
+	for i := 0; i < workers; i++ {
+		go checkTime(f, layers, timeChan, respChan)
+	}
+	time := 0
+	for {
+		select {
+		case timeChan <- time:
+			time++
+		case resp := <-respChan:
+			return resp
+		}
+	}
 }
 
 func main() {
